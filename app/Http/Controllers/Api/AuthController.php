b441436c7d395a36306d43421b30a7c1e4a85c27
Fleksis <markuss0303@gmail.com>
2025-12-23 03:22:29 +0200

    feat: added structure for auth

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        // Register method
+    }
+
+    public function login(Request $request) {
+        // Login method
+    }
+
+    public function logout(Request $request) {
+        // Logouts user
+    }
+
+    public function user(Request $request) {
+        // Returns auth user
+    }
+}
