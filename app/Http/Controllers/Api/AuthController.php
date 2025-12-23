c343bc0eed005a4bd97721b3a289e1fa76420760
Fleksis <markuss0303@gmail.com>
2025-12-23 03:46:46 +0200

    feat: added register method with auth methods

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
-        // Register method
+    public function register(RegisterRequest $request) {
+        $validated = $request->validated();
+
+        $user = User::create($validated);
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'data' => [
+                'status' => 'success',
+                'message' => 'Register successfully',
+                'data' => [
+                    'user' => new UserResource($user),
+                    'token' => $token,
+                ],
+            ],
+        ]);
     }
 
     public function login(Request $request) {
@@ -19,7 +36,7 @@ class AuthController extends Controller
         // Logouts user
     }
 
-    public function user(Request $request) {
-        // Returns auth user
+    public function user() {
+        return new UserResource(auth()->user());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/me', [AuthController::class, 'user']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/me', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
