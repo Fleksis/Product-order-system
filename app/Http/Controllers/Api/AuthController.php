90b696e0406485e9aa80a9fd8797d21337262eb9
Fleksis <markuss0303@gmail.com>
2025-12-24 01:15:13 +0200

    feat: added login method

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -14,7 +15,6 @@ class AuthController extends Controller
         $validated = $request->validated();
 
         $user = User::create($validated);
-        $token = $user->createToken('auth-token')->plainTextToken;
 
         return response()->json([
             'data' => [
@@ -22,17 +22,38 @@ class AuthController extends Controller
                 'message' => 'Register successfully',
                 'data' => [
                     'user' => new UserResource($user),
-                    'token' => $token,
+                    'token' => $user->createToken('auth-token')->plainTextToken,
                 ],
             ],
         ]);
     }
 
-    public function login(Request $request) {
-        // Login method
+    public function login(LoginRequest $request) {
+        $validated = $request->validated();
+
+        $user = User::where('email', $validated['email'])->first();
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            return response()->json([
+                'data' => [
+                    'status' => 'error',
+                    'message' => 'Invalid credentials',
+                ],
+            ], 401);
+        }
+
+        return response()->json([
+            'data' => [
+                'status' => 'success',
+                'message' => 'Login successfully',
+                'data' => [
+                    'user' => new UserResource($user),
+                    'token' => $user->createToken('auth-token')->plainTextToken,
+                ],
+            ]
+        ]);
     }
 
-    public function logout(Request $request) {
+    public function logout() {
         auth()->user()->currentAccessToken()->delete();
 
         return response()->json([
