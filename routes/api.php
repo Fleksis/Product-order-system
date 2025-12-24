9bc99bf54cf90e4f8b8cf6612d310d1d4c2d8e42
Fleksis <markuss0303@gmail.com>
2025-12-25 00:31:00 +0200

    feat: added structure for Product model

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,4 +11,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/me', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('products', ProductController::class);
 });
