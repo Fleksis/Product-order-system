1dbb3dc1a03dc4d904309f1e39720f26e8066fbe
Fleksis <markuss0303@gmail.com>
2025-12-25 04:52:47 +0200

    feat: added roles for routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\RolesEnum;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -8,9 +9,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::apiResource('products', ProductController::class)->only(['index', 'show']);
+
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/me', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::apiResource('products', ProductController::class);
+    Route::group(['middleware' => ['role:'.RolesEnum::ADMIN->value]], function () {
+        Route::apiResource('products', ProductController::class)->only(['store', 'update', 'destroy']);
+    });
 });
